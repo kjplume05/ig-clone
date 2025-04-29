b4304aee195ce19bee02053d1a02ad6fe8cc8d3c
Kaspars Jānis Plūme <kjplume05@gmail.com>
2025-04-29 10:26:40 +0300

    Added error showing in the rigistration form

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 
 const Register = () => {
+
+    // State for displaying errors
+    const [errors, setErrors] = useState({})
 
     // Defines the state that will store and change the form data
     const [formData, setFormData] = useState({
@@ -19,10 +23,13 @@ const Register = () => {
         });
     }
 
+    // Function for when the register form gets called
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
+        try{ // tries to get a response from the api
+
+            // makes a POST request to the API including the data type and the required answer type
             const response = await fetch('https://lareact.ddev.site/api/register', {
                 method: 'POST',
                 headers: {
@@ -34,8 +41,13 @@ const Register = () => {
 
             if (response.ok){
                 console.log('Register successful', response);
-            } else {
-                console.error('Register failed 1', response);
+            } else { // await is used to wait while the fetch request gets sent back and only then execute the code
+                const errorData = await response.json();
+
+                if (errorData.errors) {
+                    setErrors(errorData.errors);
+                }
+                console.error('Register failed:', errorData);
             }
         } catch (error) {
             console.log('Register failed 2', error);
@@ -57,6 +69,7 @@ const Register = () => {
                         onChange={handleChange}
                         value={formData.username}
                     />
+                    {errors.username && <p className={"text-red-500"}>{errors.username[0]}</p>}
 
                     <h2>E-mail: </h2>
                     <input
@@ -68,6 +81,7 @@ const Register = () => {
                         onChange={handleChange}
                         value={formData.email}
                     />
+                    {errors.email && <p className={"text-red-500"}>{errors.email[0]}</p>}
 
                     <h2>Password: </h2>
                     <input
@@ -79,6 +93,7 @@ const Register = () => {
                         onChange={handleChange}
                         value={formData.password}
                     />
+                    {errors.password && <p className={"text-red-500"}>{errors.password[0]}</p>}
 
                     <h2>Confirm password: </h2>
                     <input
@@ -90,6 +105,7 @@ const Register = () => {
                         onChange={handleChange}
                         value={formData.password_confirmation}
                     />
+                    {errors.password_confirmation && <p className={"text-red-500"}>{errors.password_confirmation[0]}</p>}
 
                     <button type={"submit"} className={"p-2 border-dark border-3 rounded-2xl bg-mid hover:cursor-pointer"}>Register!</button>
                 </div>
